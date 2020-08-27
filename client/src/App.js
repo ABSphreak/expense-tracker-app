1cba92b3a000f0a2322a6527816048629a8d0231
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2020-08-28 00:21:04 +0530

    ➕ Basic UI

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div>Expense Tracker</div>;
+};
+
+export default Header;
