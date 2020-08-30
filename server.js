8dfc149dd169986229d28445821d286d3ed87bab
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2020-08-30 21:16:26 +0530

    🔧 Refactor import

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 require("colors");
+require("dotenv").config({ path: "./config/config.env" });
 const express = require("express");
-const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
-dotenv.config({ path: "./config/config.env" });
 
 connectDB();
 
